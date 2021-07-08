0a4e7d45611448b589413f3d08fbf66582e6b851
Валерий Печерский <pecherskiy.v@gmail.com>
2021-07-08 17:10:58 +0300

    2.0.2 - \OkStuff\PhpNsq\Tunnel\Config

--- src/phpnsq/Tunnel/Config.php
+++ src/phpnsq/Tunnel/Config.php
@@ -11,11 +11,80 @@ class Config
 {
     public bool $initialized = false;
 
+    private int $dialTimeout = 1;
+    private array $readTimeout = [
+        'default' => 60,
+        'min' => 0.1,
+        'max' => 5 * 60,
+    ];
+    private array $writeTimeout = [
+        'default' => 1,
+        'min' => 0.1,
+        'max' => 5 * 60,
+    ];
+    private string $localAddr;
+
+    private array $lookupdPollInterval = [
+        'default' => 60,
+        'min' => 0.01,
+        'max' => 5 * 60,
+    ];
+    private array $lookupdPollJitter = [
+        'default' => 0.3,
+        'min' => 0,
+        'max' => 1,
+    ];
+
+    private array $maxRequeueDelay = [
+        'default' => 15 * 60,
+        'min' => 0,
+        'max' => 60 * 60,
+    ];
+    private array $defaultRequeueDelay = [
+        'default' => 90,
+        'min' => 0,
+        'max' => 60 * 60,
+    ];
+
+    //TODO: need to be fixed
+    private mixed $backoffStrategy;
+    private array $maxBackoffDuration = [
+        'default' => 2 * 60,
+        'min' => 0,
+        'max' => 60 * 60,
+    ];
+    private array $backoffMultiplier = [
+        'default' => 1,
+        'min' => 0,
+        'max' => 60 * 60,
+    ];
+
+    private array $maxAttempts = [
+        'default' => 5,
+        'min' => 0,
+        'max' => 65535,
+    ];
+
+    private array $lowRdyIdleTimeout = [
+        'default' => 10,
+        'min' => 1,
+        'max' => 5 * 60,
+    ];
+    private array $lowRdyTimeout = [
+        'default' => 30,
+        'min' => 1,
+        'max' => 5 * 60,
+    ];
+    private array $rdyRedistributeInterval = [
+        'default' => 5,
+        'min' => 0.001,
+        'max' => 5,
+    ];
+
     public function __construct(
         public string $host,
         public int $port
-    )
-    {
+    ) {
         $this->initialized = true;
     }
 
@@ -37,12 +106,16 @@ class Config
         return $this->$key;
     }
 
-    //check if all the value is between min and max value.
+    /**
+     * check if all the value is between min and max value.
+     * @return bool
+     * @throws Exception
+     */
     public function validate(): bool
     {
         foreach ($this as $key => $val) {
             if (is_array($val) && 3 === count($val)) {
-                if (!isset($val['default']) || !isset($val['min']) || !isset($val['max'])) {
+                if (!isset($val['default'], $val['min'], $val['max'])) {
                     throw new Exception(sprintf("invalid %s value", $key));
                 }
 
